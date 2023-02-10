f4b54217191d13f6265a0890e01ae029a166c001
YuliiaMatich <yuliia.matich@gmail.com>
2023-02-10 18:44:09 -0500

    add form

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -4,9 +4,9 @@ import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = props => {
 return <button className={classes.button}>
-  <span  className={classes.icon}><CartIcon/></span>
+  <span className={classes.icon}><CartIcon/></span>
   <span>Your cart</span>
-  <span  className={classes.badge}>3</span>
+  <span className={classes.badge}>3</span>
 </button>
 }
 
