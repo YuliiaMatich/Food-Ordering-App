fcf36f73a6233e9153968acf18a59938a5b6e58b
YuliiaMatich <yuliia.matich@gmail.com>
2023-04-12 12:50:28 -0400

    fetching meals via http

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,55 @@
+import { useState, useEffect, useCallback } from 'react';
+
 import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+
 
 const AvailableMeals = () => {
-  const mealList = DUMMY_MEALS.map((meal) => (
+
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchMealsHandler = useCallback (async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch("https://react-http-6301a-default-rtdb.firebaseio.com/meals.json");
+      
+
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+
+      const data = await response.json();
+      console.log(data);
+
+      const loadedMeals = [];
+
+      for (const key in data){
+        loadedMeals.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        })
+      }
+
+      setMeals(loadedMeals);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, [])
+
+
+  useEffect(()=> {
+    fetchMealsHandler();
+  }, [fetchMealsHandler]);
+
+  const mealList = meals.map((meal) => (
     <MealItem
       meal={meal}
       id={meal.id}
